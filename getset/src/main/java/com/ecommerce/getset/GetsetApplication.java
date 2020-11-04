18bd6071dbb3cde59228c158103add7bf2ddf16a
Lilgui <cochaiendo@gmail.com>
2020-11-04 15:44:57 -0300

    GetSet project-Creating spring structure

---
+++ getset/src/main/java/com/ecommerce/getset/GetsetApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.getset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetsetApplication.class, args);
+	}
+
+}
