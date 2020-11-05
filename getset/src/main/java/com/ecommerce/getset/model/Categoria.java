5c5ddd92f36ef38f5091d5dbdeb058349fd22d88
Rubenscode <rubens.andrade1@gmail.com>
2020-11-05 15:06:26 -0300

    Creating model and base attributes

---
+++ getset/src/main/java/com/ecommerce/getset/model/Categoria.java
@@ -0,0 +1,59 @@
+package com.ecommerce.getset.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1, max = 100)
+	private String nome;
+	
+	private boolean disponivel;
+	
+	private boolean freteGratis;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	public boolean isFreteGratis() {
+		return freteGratis;
+	}
+
+	public void setFreteGratis(boolean freteGratis) {
+		this.freteGratis = freteGratis;
+	}
+		
+}
