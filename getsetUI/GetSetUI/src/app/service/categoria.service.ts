1a1126bd217dd6e7ab5d9c92f6c826429422d329
Guilherme Máximo Endo <cochaiendo@gmail.com>
2020-12-13 12:44:38 -0300

    Creation of 'ponto jar'

--- getsetUI/GetSetUI/src/app/service/categoria.service.ts
+++ getsetUI/GetSetUI/src/app/service/categoria.service.ts
@@ -16,22 +16,22 @@ export class CategoriaService {
   }
 
   getAllCategorias(): Observable<Categoria[]> {
-    return this.http.get<Categoria[]>('http://localhost:9000/categorias', this.token)
+    return this.http.get<Categoria[]>('http://localhost:8080/categorias', this.token)
   }
 
   getByIdCategoria(id: number): Observable<Categoria> {
-    return this.http.get<Categoria>(`http://localhost:9000/tema/${id}`, this.token)
+    return this.http.get<Categoria>(`http://localhost:8080/tema/${id}`, this.token)
   }
 
   postCategoria(categoria: Categoria): Observable<Categoria> {
-    return this.http.post<Categoria>('http://localhost:9000/categorias', categoria, this.token)
+    return this.http.post<Categoria>('http://localhost:8080/categorias', categoria, this.token)
   }
 
   putCategoria(categoria: Categoria): Observable<Categoria> {
-    return this.http.put<Categoria>('http://localhost:9000/categorias', this.token)
+    return this.http.put<Categoria>('http://localhost:8080/categorias', this.token)
   }
 
   deleteCategoria(id: number) {
-    return this.http.delete(`http://localhost:9000/tema/${id}`, this.token)
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
   }
 } 
