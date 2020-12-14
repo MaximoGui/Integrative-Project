ede16a708bd5e078b1a2f067ad529a03813874be
Marcus Vinicius <marcusviniciuso@outlook.com.br>
2020-12-14 17:28:51 -0300

    creation of about uscreation of about us

--- getsetUI/GetSetUI/src/app/about/about.component.ts
+++ getsetUI/GetSetUI/src/app/about/about.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-about',
@@ -7,6 +10,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+
+  faFacebook = faFacebook
+  faInstagram = faInstagram
+  faLinkedin = faLinkedin
 
   constructor(
     private router: Router
