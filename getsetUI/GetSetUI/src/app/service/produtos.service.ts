8d62dac9612639e0dd20b409228eb20c3b50089d
Guilherme Máximo Endo <cochaiendo@gmail.com>
2021-01-19 16:52:55 -0300

    Criação da nova documentação pelo swagger

--- getsetUI/GetSetUI/src/app/service/produtos.service.ts
+++ getsetUI/GetSetUI/src/app/service/produtos.service.ts
@@ -16,30 +16,30 @@ export class ProdutosService {
   }
 
   getAllProdutos(): Observable<Produto[]> {
-    return this.http.get<Produto[]>('http://localhost:9000/produtos',this.token)
+    return this.http.get<Produto[]>('https://thegetset.herokuapp.com/produtos',this.token)
   }
 
   getByIdProduto(id: number): Observable<Produto> {
-    return this.http.get<Produto>(`http://localhost:9000/produtos/${id}`, this.token)
+    return this.http.get<Produto>(`https://thegetset.herokuapp.com/${id}`, this.token)
   }
 
   getByIdProdutos(id: number): Observable<Produto> {
-    return this.http.get<Produto>(`http://localhost:9000/produtos/${id}`, this.token)
+    return this.http.get<Produto>(`https://thegetset.herokuapp.com/${id}`, this.token)
   }
 
   postProduto(produto: Produto): Observable<Produto> {
-    return this.http.post<Produto>('http://localhost:9000/produtos', produto, this.token)
+    return this.http.post<Produto>('https://thegetset.herokuapp.com', produto, this.token)
   }
 
   putProduto(produto: Produto): Observable<Produto> {
-    return this.http.put<Produto>('http://localhost:9000/produtos', produto, this.token)
+    return this.http.put<Produto>('https://thegetset.herokuapp.com', produto, this.token)
   }
 
   deleteProduto(id: number) {
-    return this.http.delete(`http://localhost:9000/produtos/${id}`, this.token)
+    return this.http.delete(`https://thegetset.herokuapp.com/${id}`, this.token)
   }
 
   getByNomeProduto(nome: string): Observable<Produto[]> {
-    return this.http.get<Produto[]>(`http://localhost:9000/produtos/nome/${nome}`, this.token)
+    return this.http.get<Produto[]>(`https://thegetset.herokuapp.com/nome/${nome}`, this.token)
   }
 }
